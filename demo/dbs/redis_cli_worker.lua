4d28ac5e28758c887786c420ad219f7870045804
chenbo <chenbo1@shangyoo.com>
2017-09-13 11:44:49 +0800

    去掉多余的

--- demo/dbs/redis_cli_worker.lua
+++ demo/dbs/redis_cli_worker.lua
@@ -1,13 +1,11 @@
 local skynet = require "skynet"
 local redis = require "skynet.db.redis"
-local pb = require "protobuf"
 local debug_trace = require "debug_trace"
 local string_util = require "string_util"
 local config = {}
 local CMD = {}
 local redis_db
-local requst_handle = {}
-local rsp_name2id = {}
+
 
 --具体的redis链接保持和执行处理
 
