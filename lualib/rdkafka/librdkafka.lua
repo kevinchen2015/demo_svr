b65629dae62319ca38df611339ed52a9f18723aa
chenbo <chenbo1@shangyoo.com>
2017-09-30 18:12:49 +0800

    m

--- lualib/rdkafka/librdkafka.lua
+++ lualib/rdkafka/librdkafka.lua
@@ -1,8 +1,8 @@
 local librdkafka = require 'rdkafka'
 
-local librdkafka.RD_KAFKA_CONF_OK = 0
-local librdkafka.RD_KAFKA_RESP_ERR_NO_ERROR = 0
-local librdkafka.RD_KAFKA_PRODUCER = 0
-local librdkafka.RD_KAFKA_CONF_OK = 0
+librdkafka.RD_KAFKA_CONF_OK = 0
+librdkafka.RD_KAFKA_RESP_ERR_NO_ERROR = 0
+librdkafka.RD_KAFKA_PRODUCER = 0
+librdkafka.RD_KAFKA_CONF_OK = 0
 
 return librdkafka
