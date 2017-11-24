304cc5237b97aaefe02e92de87fb3ca7d27dfd70
chenbo <chenbo1@shangyoo.com>
2017-11-24 17:57:17 +0800

    顺序处理

--- demo/dbs/redis_cli_worker.lua
+++ demo/dbs/redis_cli_worker.lua
@@ -2,10 +2,11 @@ local skynet = require "skynet"
 local redis = require "skynet.db.redis"
 local debug_trace = require "debug_trace"
 local string_util = require "string_util"
+local queue = require "skynet.queue"
 local config = {}
 local CMD = {}
 local redis_db
-
+local lock
 
 --具体的redis链接保持和执行处理
 
@@ -16,8 +17,16 @@ function CMD.open(source,conf)
 	--todo reconnect
 end
 
-function CMD.execute(source,cmd,param)
+function do_request(req_info)
+	local cmd = req_info.cmd
+	local param = req_info.param
 	local ret = redis_db[cmd](redis_db,string_util.split(param," "))
+end
+
+function CMD.execute(source,cmd,param)
+	req_info.cmd = cmd
+	req_info.param = param
+	lock(do_request,req_info)
 	return ret
 end
 
@@ -28,6 +37,7 @@ skynet.start(function()
 		skynet.ret(skynet.pack(f(source, ...)))
 	end)
 
+	lock = queue()
 end)
 
 
